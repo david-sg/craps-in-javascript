97f37900e4cfbbe0b4e154c6814daecc25244b73
David M <davidmosk@gmail.com>
2019-09-27 21:00:03 +0800

    completed checks on amounts bet vs bankroll, prevent inflation

--- js/script.js
+++ js/script.js
@@ -41,14 +41,65 @@ var disableButtons = function () {};
 
 
 var betClicked = function (event) {
+// clear the warning
+	document.getElementById("win-lose").style.display = "none";
 	var betValue = event.target.value;
-	betValue = Number(betValue);
-		playerData.amountBet = playerData.amountBet + betValue;
-	// player bet can't go below 0
-	if (playerData.amountBet < 0 ) {
-			playerData.amountBet = 0; 
-		}
+// need YOLO and CLEAR conditionals
+	if (betValue === 'clear'){
+		playerData.bankroll = playerData.bankroll + playerData.amountBet;
+		playerData.amountBet = 0;
+		document.getElementById("current-bet").innerText = "$"+ playerData.amountBet;
+		showPlayerInfo();
+		return true;
+	}
 
+	if (betValue === 'yolo'){
+			if (playerData.bankroll === 0){
+				document.getElementById("win-lose").style.display = "";
+				document.getElementById("win-lose").innerText = "Nothing left to YOLO!";
+				return true;
+			}
+		playerData.amountBet = playerData.bankroll + playerData.amountBet;
+		playerData.bankroll = 0;
+		document.getElementById("current-bet").innerText = "$"+ playerData.amountBet;
+		showPlayerInfo();
+		return true;
+	}
+
+	betValue = Number(betValue);
+
+
+	//check bank balance
+	if (playerData.bankroll + betValue < 0 && betValue > 0) {
+	// show them a warning
+		document.getElementById("win-lose").style.display = "";
+		document.getElementById("win-lose").innerText = "You can not go below your bankroll!";
+		return true;
+	} 
+	// don't allow removal of bets to go negative
+	if (playerData.bankroll === 0 && betValue >0){
+		document.getElementById("win-lose").style.display = "";
+		document.getElementById("win-lose").innerText = "You can't go below your bankroll!";
+		return true;
+	} 
+
+	// make all amounts deducted end at 0...
+
+	if (betValue  <0 && (playerData.amountBet + betValue) <=0 ){
+		betValue = 0;
+		playerData.bankroll = playerData.bankroll +playerData.amountBet;
+		playerData.amountBet = 0;
+		// document.getElementById("win-lose").style.display = "";
+		// document.getElementById("win-lose").innerText = "You can not go below your current bet!";
+		// return true;
+	} 
+
+	playerData.amountBet = playerData.amountBet + betValue;
+		// decrease the players bankroll, or increase it up to the amount they have in the current bet
+	playerData.bankroll = playerData.bankroll - betValue;
+	document.getElementById("current-bet").innerText = "$"+ playerData.amountBet;
+	showPlayerInfo();
+	return true;
 }
 
 
@@ -58,44 +109,38 @@ var rollClicked = function (event) {
 }
 
 var gameStart = function (playerName) {
-showPlayerInfo();
+	showPlayerInfo();
 // get a bet
-
-
-
 }
 
 var showPlayerInfo = function () {
 document.getElementById('player-info').innerHTML = "<h3>Player Info</h3>";
 
-	    var ulInput = document.createElement('ul');
-	    var li = document.createElement("li");
-	    li.textContent = "Name: " + playerData.name;
-	    ulInput.appendChild(li);
-	    li = document.createElement("li");
-	   	li.textContent = "Bankroll: $" + playerData.bankroll;
-	   	ulInput.appendChild(li);
-	   	li = document.createElement("li");
-	   	li.textContent = "Won: $" + playerData.wins;
-	   	ulInput.appendChild(li);
-	   	li = document.createElement("li");
-	   	li.textContent = "Losses: $" + playerData.losses;
-	   	ulInput.appendChild(li);
-	   	li = document.createElement("li");
-	   	li.textContent = "Games Played: " + playerData.gamesPlayed;
-      	ulInput.appendChild(li);
+	var ulInput = document.createElement('ul');
+	var li = document.createElement("li");
+	li.textContent = "Name: " + playerData.name;
+	ulInput.appendChild(li);
+	li = document.createElement("li");
+	li.textContent = "Bankroll: $" + playerData.bankroll;
+	ulInput.appendChild(li);
+	li = document.createElement("li");
+	li.textContent = "Won: $" + playerData.wins;
+	ulInput.appendChild(li);
+	li = document.createElement("li");
+	li.textContent = "Losses: $" + playerData.losses;
+	ulInput.appendChild(li);
+	li = document.createElement("li");
+	li.textContent = "Games Played: " + playerData.gamesPlayed;
+	ulInput.appendChild(li);
 
-		var thePlayer = document.getElementById("player-info"); 
-		thePlayer.appendChild(ulInput); 
+	var thePlayer = document.getElementById("player-info"); 
+	thePlayer.appendChild(ulInput); 
 };
 
 
 //hide areas on load
 var hideLoadAreas = function () {
-	// document.getElementById("dice-area").style.visibility = "hidden";
 	document.getElementById("display-come-out-number").style.visibility = "hidden";
-	// document.getElementById('win-lose').innerHTML = "";
-	// document.getElementById("win-lose").style.visibility = "collapse";
 	document.getElementById("win-lose").style.display = "none";
 
 };
@@ -104,8 +149,7 @@ var hideLoadAreas = function () {
 
 //  get player name input, and validate. If empty ask again...
 var nameFunction = function (){
-
-		var input = document.querySelector('#name-input'); 
+	var input = document.querySelector('#name-input'); 	
 	var inputValue = input.value;
 	if (inputValue.replace(/\s+/g, '').length == 0) {
 		getPlayerName();
@@ -117,11 +161,7 @@ var nameFunction = function (){
 }
 
 var getName = function () {
-	// empty the area and then display asking for name in info section
-
-	// document.getElementById("info-section").style.visibility = "visible";
-
-	// build the imput and submit button...
+	// build the input and submit button...
     var infoSection = document.getElementById('info-section');
     var nameInput = document.createElement('input');
 	nameInput.setAttribute('placeholder', "What's your name?");
@@ -129,10 +169,9 @@ var getName = function () {
 	nameInput.setAttribute('id', "name-input");
 	infoSection.appendChild(nameInput);
 	document.querySelector('#name-input').addEventListener('change', nameFunction); 
-
-
 }
 
+// lets get this started....
 getName();
 
 
